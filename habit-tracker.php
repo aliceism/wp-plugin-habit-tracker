6738bf9b97011554f845e6aaa6a0c7d9c298346c
aliceism <alice7ism@gmail.com>
2026-01-22 14:01:48 +0200

    Initial plugin structure

---
+++ habit-tracker.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Habit Tracker
+ * Description: Плъгин за проследяване на здравословни навици.
+ * Version: 1.0
+ * Author: Alice Ismail
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
